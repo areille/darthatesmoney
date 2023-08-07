752fba7212187ef7b25002a4876d15ed26155908
areille <areille@mailbox.org>
2023-08-07 16:21:54 +0200

    feat: add create project

--- lib/src/darthatesmoney.dart
+++ lib/src/darthatesmoney.dart
@@ -20,19 +20,49 @@ class Darthatesmoney {
   /// The ihatemoney.org private code.
   final String privateCode;
 
-  /// This ihatemoney.org uri.
-  Uri get projectUri =>
-      Uri.parse('https://ihatemoney.org/api/projects/$projectName');
+  static const _baseAPIUrl = 'https://ihatemoney.org/api/';
+
+  /// This ihatemoney.org project uri.
+  Uri get projectUri => Uri.parse('${_baseAPIUrl}projects/$projectName');
 
   /// The ihatemoney.org uri, containing the HTTP Basic info for auth.
   Uri authenticatedUri(Uri uri) =>
       uri.replace(userInfo: '$projectName:$privateCode');
 
+  // ========
+  // PROJECTS
+  // ========
+
   /// Returns basic project info.
+  ///
+  /// [Docs](https://ihatemoney.readthedocs.io/en/latest/api.html#getting-information-about-the-project)
   Future<ProjectInfo> getProjectInfo() async {
     final response = await http.get(authenticatedUri(projectUri));
 
     return ProjectInfo.fromJson(response.body);
+  }
+
+  /// Creates a project. Returns the newly created project id.
+  ///
+  /// [Docs](https://ihatemoney.readthedocs.io/en/latest/api.html#creating-a-project)
+  Future<String> createProject({
+    required String name,
+    required String id,
+    required String password,
+    required String contactEmail,
+    String? defaultCurrency,
+  }) async {
+    final response = await http.post(
+      Uri.parse('${_baseAPIUrl}projects'),
+      body: {
+        'name': name,
+        'id': id,
+        'password': password,
+        'contact_email': contactEmail,
+        if (defaultCurrency != null) 'default_currency': defaultCurrency,
+      },
+    );
+    return response.body;
   }
 
   /// Lists a project members. See [this link](https://ihatemoney.readthedocs.io/en/latest/api.html#members)
